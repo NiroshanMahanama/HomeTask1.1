6065ebc59b9558dab73620b636e8bbd404b47c34
NiroshanMahanama <nmahanama@gmail.com>
2022-11-21 14:43:11 +0100

    FEature1 commit1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -7,6 +7,7 @@ namespace HomeTask1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello whole world!");
+            Console.WriteLine("Feature branch 1 - develpmemt 1");
         }
     }
 }
